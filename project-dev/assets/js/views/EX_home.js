b8c260bae639dc0c835d230beca4adc9c393e50f
Hrag Chanchanian <hragatron@gmail.com>
2015-04-26 22:46:25 -0700

    Removed some views

---
+++ project-dev/assets/js/views/EX_home.js
@@ -0,0 +1,14 @@
+// views/EX_home.js
+
+define([
+	'jquery',
+	'mustache',
+	'helpers/EX_before'
+], function($, Mustache, Before) {
+	return function(){
+		
+		// View Stuff	
+	
+	};
+});
+
--- project-dev/assets/js/views/personal.js
+++
@@ -1,6 +0,0 @@
-// views/personal.js
-
-define(['jquery', 'json!projects.json'], function($, probject) {
-	var blah = {'test': probject};
-	return blah;
-});
